a98d13422d64464ee7913efc6f5caebe0fda9b96
https://github.com/fabianaopalmeida <iana.opa@gmail.com>
2026-09-24 16:38:16 -0300

    corrige uso de eval sobre entrada do usuário e innerHTML com entrada do usuário sem sanitização

--- src/script.js
+++ src/script.js
@@ -21,9 +21,11 @@ function addTask() {
     const input = document.getElementById('new-task');
     const output = document.getElementById('output');
 
-    output.innerHTML = '<li>' + input.value + '</li>';
+    const li = document.createElement('li');
+li.innerText = input.value;
+output.appendChild(li);
 
-    eval('console.log("Tarefa adicionada: ' + input.value + '")');
+    console.log("Tarefa adicionada: " + input.value);
 
     input.value = '';
 }
